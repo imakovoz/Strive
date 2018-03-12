4c47dea1f0af67653db33c59cd00ca9b24ef758f
Ilya Makovoz <imakovoz93@gmail.com>
2018-03-12 10:31:02 -0400

    initial dashboard styling

--- frontend/components/dashboard/post_item.jsx
+++ frontend/components/dashboard/post_item.jsx
@@ -1,91 +1,174 @@
-import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
 ];
 const formatDate = (d, t) => {
   const date = new Date(d);
   let hours = date.getHours();
-  let minutes = Math.round(date.getMinutes()/10) * 10;
+  let minutes = Math.round(date.getMinutes() / 10) * 10;
   if (minutes === 60) {
     hours++;
     minutes = 0;
   }
-  let ampm = hours >= 12 ? 'PM' : 'AM';
+  let ampm = hours >= 12 ? "PM" : "AM";
   hours = hours % 12;
   hours = hours ? hours : 12;
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  const strTime = hours + ':' + minutes + ' ' + ampm;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  const strTime = hours + ":" + minutes + " " + ampm;
   const month = monthNames[date.getMonth()];
-  return month + " " + date.getDate() + ", " + date.getFullYear() + " at " + strTime;
+  return (
+    month + " " + date.getDate() + ", " + date.getFullYear() + " at " + strTime
+  );
 };
 
 class PostItem extends React.Component {
-
   render() {
     let linker = null;
     let details = null;
-    const dist_uom_conv = {miles: "mi", kilometers: "km", meters: "m", yards: "yd"};
-    if (this.props.post.distance) {
+    const dist_uom_conv = {
+      miles: "mi",
+      kilometers: "km",
+      meters: "m",
+      yards: "yd"
+    };
+    if (this.props.post.activity) {
       linker = "workouts";
       let pace = null;
-      let base  = this.props.post.duration / this.props.post.distance;
+      let base = this.props.post.duration / this.props.post.distance;
       if (base >= 360) {
-        let hr = Math.floor(base/360);
-        let min = ("0" + (base % 360) /60).slice(-2);
+        let hr = Math.floor(base / 360);
+        let min = ("0" + (base % 360) / 60).slice(-2);
         let sec = ("0" + base % 60).slice(-2);
         pace = hr + ":" + min + ":" + sec;
-      } else if (base >= 60){
-        let min = Math.floor(base/60);
+      } else if (base >= 60) {
+        let min = Math.floor(base / 60);
         let sec = ("0" + base % 60).slice(-2);
         pace = min + ":" + sec;
-      } else if (base >= 1){
+      } else if (base >= 1) {
         pace = base + "s";
       } else {
         pace = "1s";
       }
+      let movingTime = null;
+      if (this.props.post.duration) {
+        movingTime = (
+          <div>
+            <span className="entry-detail-label">Moving Time</span>
+            <a className="feed-item-details">
+              {Math.floor(this.props.post.duration / 360)}h{" "}
+              {Math.floor((this.props.post.duration % 360) / 60)}m
+            </a>
+          </div>
+        );
+      } else {
+        movingTime = <div />;
+      }
+      let distance = null;
+      if (this.props.post.distance) {
+        distance = (
+          <div>
+            <span className="entry-detail-label">Distance</span>
+            <a className="feed-item-details">
+              {this.props.post.distance}{" "}
+              {this.props.post.distance_uom.slice(0, -1)}
+            </a>
+          </div>
+        );
+      } else {
+        distance = <div />;
+      }
+      let entryPace = null;
+      if (this.props.post.distance) {
+        entryPace = (
+          <div>
+            <span className="entry-detail-label">Average Pace</span>
+            <a className="feed-item-details">
+              {pace}/{dist_uom_conv[this.props.post.distance_uom]}
+            </a>
+          </div>
+        );
+      } else {
+        entryPace = <div />;
+      }
+
+      let spacers = null;
+      if (this.props.post.distance) {
+        spacers = <hr />;
+      } else {
+        spacers = <div />;
+      }
+
       details = (
         <div className="feed-item-content-details">
-          <a className="feed-item-details">{this.props.post.body}</a>
-          <a className="feed-item-details">{Math.floor(this.props.post.duration/360)}h {Math.floor((this.props.post.duration % 360)/60)}m</a>
-          <a className="feed-item-details">{this.props.post.distance} {this.props.post.distance_uom.slice(0, -1)}</a>
-          <a className="feed-item-details">{(pace)}/{dist_uom_conv[this.props.post.distance_uom]}</a>
+          {movingTime}
+          {spacers}
+          {distance}
+          {spacers}
+          {entryPace}
         </div>
-    );
+      );
     } else {
       linker = "posts";
-      details = <Link to={`/users/${this.props.user.id}/${linker}/${this.props.post.id}`}
-        className="feed-item-body">{this.props.post.body}</Link>;
+      details = (
+        <Link
+          to={`/users/${this.props.user.id}/${linker}/${this.props.post.id}`}
+          className="feed-item-post-body"
+        >
+          {this.props.post.body}
+        </Link>
+      );
     }
     return (
-      <div className="feed-item-container-div">
-        <div className="feed-item-prof-pic-div">
+      <div className="post-item-container">
+        <div className="entry-header">
           <Link to={`/users/${this.props.user.id}`}>
-            <img src={`${window.profPic}`} height="50"
-            width="50" className="dropbtn" />
+            <img src={`${window.profPic}`} height="40" width="40" />
           </Link>
+          <div className="entry-header-details">
+            <Link
+              to={`/users/${this.props.user.id}`}
+              className="feed-item-post-user"
+            >
+              {this.props.user.firstname} {this.props.user.lastname}
+            </Link>
+            <span className="feed-item-date-span">
+              {formatDate(this.props.post.created_at)}
+            </span>
+          </div>
         </div>
-        <div className="feed-item-content-div">
-          <div className="feed-item-meta-div">
-<Link to={`/users/${this.props.user.id}`} className="feed-item-post-user">
-{this.props.user.firstname} {this.props.user.lastname}</Link>
-          <span className="feed-item-date-span">
-            {formatDate(this.props.post.created_at)}
-          </span>
+        <div className="entry-body">
+          <img src={`${window.runningShoe}`} height="24" width="24" />
+          <div>
+            <Link
+              to={`/users/${this.props.user.id}/${linker}/${
+                this.props.post.id
+              }`}
+              className="feed-item-title"
+            >
+              {this.props.post.title}
+            </Link>
+            {details}
           </div>
-
-          <div className="feed-item-post-div">
-<Link to={`/users/${this.props.user.id}/${linker}/${this.props.post.id}`}
-className="feed-item-title">{this.props.post.title}</Link>
-
-          { details }
-
-          </div>
+        </div>
+        <div className="entry-footer">
+          <img src={`${window.like}`} height="16" width="17" />
+          <img src={`${window.comment}`} height="16" width="17" />
         </div>
       </div>
     );
-
-}
+  }
 }
 export default PostItem;
