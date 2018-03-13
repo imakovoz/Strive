6ba7469a7b891692844e7f838efe75e5733bdab8
Ilya Makovoz <imakovoz93@gmail.com>
2018-03-12 20:15:08 -0400

    add header styling

--- frontend/components/routes/create/header.jsx
+++ frontend/components/routes/create/header.jsx
@@ -23,9 +23,7 @@ export default class RouteHeader extends React.Component {
             <div>Redo Btn</div>
           </div>
           <div>
-            <div onClick={this.props.openModal} id="save-route">
-              Save
-            </div>
+            {this.props.savebtn}
           </div>
         </div>
       </div>
